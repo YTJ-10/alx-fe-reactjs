84f9f807881071262796513dae36ff2768bb42fe
YTJ-10 <yasirtijani913@gmail.com>
2025-11-25 11:41:22 +0100

    Initial commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,8 @@
+// src/components/UserProfile.jsx
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 max-w-xs mx-auto my-8 rounded-lg shadow-lg 
-                    sm:p-6 sm:max-w-sm sm:my-12
+                    sm:p-4 sm:max-w-sm sm:my-12
                     md:p-8 md:max-w-sm md:my-20">
       <img 
         src="https://via.placeholder.com/150" 
